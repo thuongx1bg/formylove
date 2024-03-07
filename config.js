3a08bc2c4e56ea7a45df112d9dd1ed09cf501673
thuongx1bg <thuongx1bg@gmail.com>
2024-03-07 23:39:13 +0700

    a

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Em';
 const giftUrl = 'https://scontent.fhan14-2.fna.fbcdn.net/v/t39.30808-6/424828218_3614632155469758_7380911909607046738_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGU_-Urs00FFo6CUUcebPDkQYVKeRQyKsZBhUp5FDIqxvNjesLZUTJB7e6lMdHQAGNfuDbfDogVI2PeHfmgp69E&_nc_ohc=0r5BhGFP3B8AX9poANW&_nc_ht=scontent.fhan14-2.fna&oh=00_AfDRQDOGDoffWv-ONdxXM1CTz2L6KqPZBuhB-ucOp6Wm1g&oe=65EC3993';
-const eventName = 'Chúc mừng 20-10';
+const eventName = 'Chúc mừng 8-3';
 const titleCard = 'Tặng cho em bé';
-const contentCard = 'Chúc em bé của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc em bé của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
